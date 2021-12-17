1b143b752d165a2c6ee5d4c384d57123463709fd
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-12-18 04:41:19 +0600

    feat: add quiz data model

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,21 @@ import { CategoriesPage } from "./categoriesPage";
 import { GameFieldPage } from "./gameFieldPage";
 import { GameOverPage } from "./gameOverPage";
 import { StartPage } from "./startPage";
+import { QuizDataModel } from "./quizDataModel";
 
 export class App extends Control {
+  model: QuizDataModel;
   constructor(parentNode: HTMLElement) {
     super(parentNode);
-    this.mainCycle();
+    // preloader
+    const preloader = new Control(this.node, "div", "", "loading...");
+    this.model = new QuizDataModel();
+    this.model.build().then((result) => {
+      preloader.destroy();
+      console.log(result.data);
+      // main
+      this.mainCycle();
+    });
   }
 
   private gameCycle(gameName: string, categoryIndex: number) {
