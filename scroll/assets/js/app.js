c8fd00c72a010d2dfc5762ef043603b5daad9e14
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-01-22 04:29:28 +0600

    feat: add js a little bit

--- scroll/assets/js/app.js
+++ scroll/assets/js/app.js
@@ -3,6 +3,7 @@ const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 const scrollLink = document.querySelectorAll(".scroll-link");
 const topLink = document.querySelector(".top-link");
+const navbar = document.getElementById("nav");
 
 burger.addEventListener("click", function () {
   const linksHeight = links.getBoundingClientRect().height;
@@ -18,15 +19,35 @@ scrollLink.forEach((el) => {
   el.addEventListener("click", function (e) {
     e.preventDefault();
     const ancor = el.getAttribute("href").slice(1);
-    const elem = document.getElementById(ancor);
-    let position = elem.offsetTop;
+    const element = document.getElementById(ancor);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
     window.scrollTo(0, position);
     linksContainer.style.height = 0;
   });
 });
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY >= 300) {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    console.log("helo");
+
     topLink.classList.add("show-link");
   } else {
     topLink.classList.remove("show-link");
